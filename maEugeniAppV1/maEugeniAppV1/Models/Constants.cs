bd94da126a2f20c5a572a0332ab7e9ecbcbdc247
ULISES MARTINEZ BUENDIA <mb_ulises@hotmail.com>
2017-07-28 21:37:58 -0500

    Cambios 28 julio

---
+++ maEugeniAppV1/maEugeniAppV1/Models/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maEugeniAppV1.Models
+{
+    class Constants
+    {
+        public static bool IsDev = true;
+    }
+}
---
+++ maEugeniAppV1/maEugeniAppV1/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maEugeniAppV1.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+        public User(string UserName, string Password)
+        {
+            this.Username = UserName;
+            this.Password = Password;
+        }
+
+    }
+}
